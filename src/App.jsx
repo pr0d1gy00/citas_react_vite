175d95f9fe19cb85e0c47c201ae2cd26114d48d8
prod1gy00 <calitoalejandro184@gmail,com>
2024-02-18 13:53:44 -0400

    modificacion al app.jsx en el import de header

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from "react"
-import Header from "./components/header"
+import Header from "./components/Header"
 import Formulario from "./components/Formulario"
 import ListadoPaciente from "./components/ListadoPaciente"
 
